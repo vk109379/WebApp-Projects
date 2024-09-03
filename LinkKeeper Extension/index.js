3c86c292bbffbf01f75fd489192907e3af339d72
Vinay Kumar <vk109379@gmail.com>
2024-09-03 22:11:09 +0530

    modified:   LinkKeeper Extension/index.js 	modified:   LinkKeeper Extension/manifest.json

--- LinkKeeper Extension/index.js
+++ LinkKeeper Extension/index.js
@@ -1,36 +1,36 @@
-let myLeads = []
+let myLinks = []
 const inputEl = document.getElementById("input-el")     //input box 
 const inputBtn = document.getElementById("input-btn")   //save button
 const tabBtn = document.getElementById("tab-btn")       // Save tab button
 const deleteBtn = document.getElementById("delete-btn")  //Delete all button
 const ulEL = document.getElementById("ul-el")           //unordered list
 //Fetching stored values from local storage and converting it into array 
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+const linksFromLocalStorage = JSON.parse( localStorage.getItem("myLinks"))
 
 
-if(leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+if(linksFromLocalStorage){
+    myLinks = linksFromLocalStorage
+    render(myLinks)
 }
 
 tabBtn.addEventListener("click", function(){
     //fetching the value in adress bar of current tab and current window
     chrome.tabs.query({active: true, currentWindow: true},function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
+        myLinks.push(tabs[0].url)
+        localStorage.setItem("myLinks", JSON.stringify(myLinks))
+        render(myLinks)
     })
 })
 
 //Function to take array elements and convert them into list of clicable links and render on screen
-function render(leads){ 
+function render(links){ 
     let listItems = ""
-    for(let i = 0; i < leads.length; i++){
+    for(let i = 0; i < links.length; i++){
         //Multiline string 
         listItems += `
             <li>            
-                <a target ="_blank" href= "${leads[i]}"> 
-                    ${leads[i]} 
+                <a target ="_blank" href= "${links[i]}"> 
+                    ${links[i]} 
                 </a>
             </li>`
     }
@@ -38,17 +38,17 @@ function render(leads){
 }
 
 inputBtn.addEventListener("click" , function(){
-    myLeads.push(inputEl.value)
+    myLinks.push(inputEl.value)
     inputEl.value = ""
-    //saving myLeads array into  local storage by making all elements string as local storage support only string format.
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    //saving myLinks array into  local storage by making all elements string as local storage support only string format.
+    localStorage.setItem("myLinks", JSON.stringify(myLinks))
+    render(myLinks)
 })
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()    //clearing local storage
-    myLeads = []
-    render(myLeads)
+    myLinks = []
+    render(myLinks)
     deleteBtn.textContent = "DELETE ALL"
 });
 
